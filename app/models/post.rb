28bae2e6fba5075201a14020a3bfcdeb0738fefc
Zachary Soliman <zachsoliman1@gmail.com>
2022-09-06 10:46:12 -0400

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait?
+
+    CLICKBAIT_PATERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATERNS.none? { |pat| pat.match title }
+         errors.add(:title, "must be clickbait")
+        end
+    
+    end
 end
